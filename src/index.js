f7b378a1923d6b7f79a45fcc391ab2f6400ac729
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-11 00:09:28 +0200

    begin index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/main.css';
+import { fetch } from './js/fetch';
+import { markup } from './js/markup';
+import Notiflix from 'notiflix';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
