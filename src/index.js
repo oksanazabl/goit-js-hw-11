9bab71f71987fe31c0493ff78f6e5a84fb8e92e4
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-15 15:32:40 +0200

    add elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import './css/main.css';
 import { fetch } from './js/fetch';
-import { markup } from './js/markup';
+import { createCard } from './js/markup';
 import Notiflix from 'notiflix';
 // Описаний в документації
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.btn-add-load');
+
+searchForm.addEventListener('submit', onSearchForm);
+btnLoadMore.addEventListener('click', onBtnLoadMore);
+
+let page = 1;
+let simpleLightBox;
+let q = '';
+const perPage = 40;
+
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  q = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  //   btnLoadMore.classList.add('hidden');
+
+  if (q === '') {
+    alertNoEmptySearch();
+    return;
+  }
+}
+
+function onBtnLoadMore() {
+  page += 1;
+  simpleLightBox.destroy();
+}
